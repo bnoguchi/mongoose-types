fd15c51f89488d8af057cc7aceed9ebb23e943d5
Shayne Fitzgerald <slfitzge@stetson.edu>
2015-09-21 11:04:34 -0400

    added useage case example

---
+++ usageExample.js
@@ -0,0 +1,30 @@
+var mongoose = require('mongoose');
+var mongoosetypes = require('mongoose-types');
+
+exports.init = function(db){
+
+  //Load the timestamps plugin
+  mongoose.plugin(mongoosetypes.useTimestamps);
+  mongoosetypes.loadTypes(mongoose);
+
+  exports.errorReport = db.model('ErrorReport', new mongoose.Schema({
+
+    username : String,
+    associatedApplication : String,
+    comments : String,
+    //client email address
+    email : mongoose.Schema.Types.Email,
+    //Intranet/Internet URI for crash dump file
+    crashDumpFileURI : mongoose.Schema.Types.URL,
+    //Validation signature for the dump file.
+    crashDumpFileSignature : mongoose.Schema.Types.MD5,
+    //Connection data
+    submittedFrom : {
+      usesIPV6 : Boolean,
+      v6 : mongoose.Schema.Types.IPv6,
+      v4 : mongoose.Schema.Types.IPv4,
+      deviceMacAddress : mongoose.Schema.Types.MACAddress,
+    },
+  }));
+};
+
